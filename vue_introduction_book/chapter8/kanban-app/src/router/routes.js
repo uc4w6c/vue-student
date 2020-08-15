7a5650e09424fa66e6368f85c0e1e359d477fff6
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2020-08-15 17:07:57 +0900

    KbnLoginViewのテストで失敗するがひとまずこの状態でコミット

--- vue_introduction_book/chapter8/kanban-app/src/router/routes.js
+++ vue_introduction_book/chapter8/kanban-app/src/router/routes.js
@@ -1,6 +1,6 @@
 import KbnBoardView from '@/components/templates/KbnBoardView.vue'
 import KbnLoginView from '@/components/templates/KbnLoginView.vue'
-import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailView.vue'
+import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal.vue'
 
 export default [{
   path: '/',
---
+++ vue_introduction_book/chapter8/kanban-app/test/unit/specs/components/templates/KbnLoginView.spec.js
@@ -0,0 +1,111 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import KbnLoginView from '@/components/templates/KbnLoginView.vue'
+import { expect } from 'chai'
+
+// ローカルなVueコンストラクタを作成
+const localVue = createLocalVue()
+
+// ローカルなVueコンストラクタにVuexをインストール
+localVue.use(Vuex)
+
+describe('KbnLoginView', () => {
+  let actions
+  let $router
+  let store
+  let LoginFormComponentStub
+
+  // `KbnLoginForm`コンポーネントのログインボタンのクリックをトリガーするヘルパー関数
+  const triggerLogin = (loginView, target) => {
+    const loginForm = loginView.find(target)
+    loginForm.vm.onlogin('foo@domain.com', '12345678')
+  }
+
+  beforeEach(() => {
+    // KbnLoginFormコンポーネントのスタブの設定
+    LoginFormComponentStub = {
+      name: 'KbnLoginForm',
+      props: ['onlogin'],
+      render: h => h('p', ['login form'])
+    }
+  })
+
+  // Vue Routerのモック設定
+  $router = {
+    push: sinon.spy()
+  }
+
+  // loginアクションの動作確認のためのVuex周りの設定
+  actions = {
+    login: sinon.stub() // loginアクションのモック
+  }
+  store = new Vuex.Store({
+    state: {},
+    actions
+  })
+
+  describe('ログイン', () => {
+    let loginView
+    describe('成功', () => {
+      beforeEach(() => {
+        loginView = mount(KbnLoginView, {
+          mocks: { $router },
+          stubs: {
+            'kbn-login-form': LoginFormComponentStub
+          },
+          store,
+          localVue
+        })
+      })
+
+      it('ボードページのルートにリダイレクトすること', done => {
+        // loginアクションを成功とする
+        actions.login.resolves()
+
+        triggerLogin(loginView, LoginFormComponentStub)
+
+        // プロミスのフラッシュ
+        loginView.vm.$nextTick().then(() => {
+          expect($router.push.called).to.equal(true)
+          expect($router.push.args[0][0].path).to.equal('/')
+        }).then(done, done)
+      })
+    })
+
+    // TOOO: 以下エラーになるが一旦放置する
+    describe('失敗', () => {
+      beforeEach(() => {
+        loginView = mount(KbnLoginView, {
+          stubs: {
+            'kbn-login-form': LoginFormComponentStub
+          },
+          store,
+          localVue
+        })
+        sinon.spy(loginView.vm, 'throwReject') // spyでラップ
+      })
+
+      afterEach(() => {
+        loginView.vm.throwReject.restore() // spyのラップ解除
+      })
+
+      it('エラー処理が呼び出されること', done => {
+        // loginアクションを失敗とする
+        const message = 'login failed'
+        actions.login.rejects(new Error(message))
+
+        triggerLogin(loginView, LoginFormComponentStub)
+
+        // プロミスのフラッシュ
+        // loginView.vm.$nextTick().catch(() => {
+        loginView.vm.$nextTick().then(() => {
+          const callInfo = loginView.vm.throwReject
+          expect(callInfo.called).to.equal(true)
+          expect(callInfo.args[0][0].message).to.equal(message)
+          // done()
+        // })
+        }).then(done, done)
+      })
+    })
+  })
+})
